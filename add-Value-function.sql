7b9e765908ec29b138c598f6b3fdddb435204b50
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-01 13:56:31 +0530

    Add Value function to insert student marks into Student_Marks table

---
+++ add-Value-function.sql
@@ -0,0 +1,8 @@
+CREATE DEFINER=`root`@`localhost` FUNCTION `InsertStudentMarks`(sID varchar(10), m int, s int, e int ) RETURNS varchar(50) CHARSET utf8mb4
+    DETERMINISTIC
+BEGIN
+ insert into Student_Marks (Student_ID, Math, Science, English)
+ values ('S105', 98, 89, 91);
+ 
+RETURN 'Inserted Successfully';
+END
