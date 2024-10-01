2ecfdaf8687bb6a375b7f8508bd8d4208408cba0
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-10-01 13:56:27 +0530

    Create temporary tables for calculating total marks in different subjects

---
+++ Student_DB.sql
@@ -0,0 +1,19 @@
+use student_db;
+
+CREATE TEMPORARY TABLE TOTAL_S AS
+SELECT Student_ID, (Math + Science + English) AS TotalMarks
+FROM Student_Marks;
+
+select * from TOTAL_S;
+
+create temporary table Total_MathsScience AS
+Select Student_ID, (Math+Science) as TotalMarksofMnS
+from Student_Marks;
+
+select * from Total_MathsScience;
+
+create temporary table TotalOfMaths_English As 
+Select Student_ID , (Math + English) as Totals 
+from Student_Marks;
+
+select * from TotalOfMaths_English;
